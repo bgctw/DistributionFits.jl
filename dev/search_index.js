28b1796f0fdabdc846c3078bba5bfe63f1175d7a
Documenter.jl <documenter@juliadocs.github.io>
2021-12-30 07:30:44 +0000

    build based on 3aaabab

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DistributionFits","category":"page"},{"location":"#DistributionFits","page":"Home","title":"DistributionFits","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DistributionFits.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DistributionFits]","category":"page"}]
+}
