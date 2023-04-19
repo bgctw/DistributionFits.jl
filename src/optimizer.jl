310fe703e91919907134b318c5fb5d3e0141d395
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2023-04-19 08:49:10 +0200

    add Optim to Extras in project.toml

--- src/optimizer.jl
+++ src/optimizer.jl
@@ -11,7 +11,8 @@ function `f` on bounded interval `[lower,upper]`:
 Returning an object with fields `minimizer` and `converged`.
 
 `DistributionFits.set_optimizer(::AbstractDistributionFitOptimizer)` 
-sets the AbstractDistributionFitOptimizer used in calling the optimize function. 
+sets the specific AbstractDistributionFitOptimizer that is used 
+throughout the package for calling the optimize function. 
 Specializing this function with a concrete type, allows using different
 optimization packages. 
 
@@ -20,11 +21,14 @@ package is in scope, the
 `OptimOptimizer` is set, which implements the interface by using Optim's 
 [optimize](https://julianlsolvers.github.io/Optim.jl/stable/#user/minimization/#minimizing-a-univariate-function-on-a-bounded-interval) function.
 
-Hence, the module using DistribuitonFits.jl has to either
+Hence, the module using `DistributionFits.jl` has to either
 
 - explicitly invoke `using Optim`, or
 - call `set_optimizer` with the concrete subtype of `AbstractDistributionFitOptimizer`
-  for which the corresponding optimize method is implemented.
+  for which the corresponding `optimize` method is implemented. 
+    
+Developers implementing usage of a different specific optimizer see code in 
+ext/DistributionFitsOptimExt.
 """
 abstract type AbstractDistributionFitOptimizer end
 struct NotSetOptimizer <: AbstractDistributionFitOptimizer; end
