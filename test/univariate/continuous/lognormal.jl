914e58342c354b6b5d8d3eee3cfb38903ed240d1
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2023-09-26 10:34:59 +0200

    base lognormal tests on test_univariate_fits

--- test/univariate/continuous/lognormal.jl
+++ test/univariate/continuous/lognormal.jl
@@ -2,72 +2,19 @@
     d = LogNormal(1,0.6)
     # note: did not specify type parameter
     test_univariate_fits(d,LogNormal)
-    # handle not giving variance
-    @test_throws Exception fit(typeof(d), Moments(3.2))
-
 end;
 
-@testset "fit moments Float32" begin
+@testset "Float32" begin
     d = LogNormal(1f0,0.6f0)
-    @test partype(d) === Float32
     test_univariate_fits(d)
 end;
 
-@testset "fit to quantilepoint and mean" begin
-    d = LogNormal(1.,1.)
-    m = log(mean(d))
-    qp = @qp(quantile(d,0.95),0.95)
-    d_fit = fit_mean_quantile(LogNormal, mean(d), qp)
-    @test d_fit ≈ d
-    d_fit = fit(LogNormal, mean(d), qp, Val(:mean))
-    @test d_fit ≈ d
-    # with lower quantile
-    qp = @qp(quantile(d,0.05),0.05)
-    d_fit = fit_mean_quantile(LogNormal, mean(d), qp)
-    @test d_fit ≈ d
-    # very close to mean can give very different results:
-    qp = @qp(mean(d)-1e-4,0.95)
-    d_fit = fit_mean_quantile(LogNormal, mean(d), qp)
-    @test mean(d_fit) ≈ mean(d) && quantile(d_fit, qp.p) ≈ qp.q
-    #
-    #qp32 = @qp(Float32(quantile(d,0.95f0)),0.95f0)
-    qp = @qp(quantile(d,0.95f0),0.95)
-    dfit32 = fit_mean_quantile(LogNormal, Float32(mean(d)), qp)
-    @test partype(dfit32) === Float32
-end;
-@testset "fit to quantilepoint and mode" begin
-    d = LogNormal(1,1)
-    m = log(mode(d))
-    qp = @qp(quantile(d,0.95),0.95)
-    d_fit = fit_mode_quantile(LogNormal, mode(d), qp)
-    @test d_fit ≈ d
-    d_fit = fit(LogNormal, mode(d), qp, Val(:mode))
-    @test d_fit ≈ d
-    # with lower quantile
-    qp = @qp(quantile(d,0.025),0.025)
-    d_fit = fit_mode_quantile(LogNormal, mode(d), qp)
-    @test mode(d_fit) ≈ mode(d) && quantile(d_fit, qp.p) ≈ qp.q
-    #
-    qp32 = @qp(Float32(quantile(d,0.25f0)),0.25f0)
-    dfit32 = fit_mode_quantile(LogNormal, Float32(mode(d)), qp32)
-    @test partype(dfit32) === Float32
-end;
-@testset "fit to quantilepoint and median" begin
-    d = LogNormal(1,1)
-    qp = @qp(quantile(d,0.95),0.95)
-    d_fit = fit(LogNormal, median(d), qp, Val(:median))
-    @test d_fit ≈ d
-    #
-    # median does not define type
-    #qp32 = @qp(Float32(quantile(d,0.25f0)),0.25f0)
-    #dfit32 = fit(LogNormal, Float32(median(d)), qp32, Val(:median))
-    dfit32 = fit(LogNormal{Float32}, median(d), qp, Val(:median))
-    @test partype(dfit32) === Float32
-end;
 
 @testset "Σstar" begin
     ss = Σstar(4.5)
     @test ss() == 4.5
+    @test eltype(Σstar(4.5f0)) == Float32
+    @test eltype(Σstar{Float32}) == Float32
 end;
 
 @testset "fit to mean and Σstar" begin
